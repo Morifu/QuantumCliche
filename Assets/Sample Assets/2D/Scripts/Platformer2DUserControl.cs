b30a94fb399fb6d45488ca0c99991db093f15800
Morifu <morifek@gmail.com>
2014-06-30 16:35:27 +0200

    Initial Commit

---
+++ Assets/Sample Assets/2D/Scripts/Platformer2DUserControl.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(PlatformerCharacter2D))]
+public class Platformer2DUserControl : MonoBehaviour 
+{
+	private PlatformerCharacter2D character;
+    private bool jump;
+	InputVCR vcr;
+	bool useVCR = false;
+
+	void Awake()
+	{
+		character = GetComponent<PlatformerCharacter2D>();
+		vcr = GetComponent<InputVCR>();
+		useVCR = vcr != null;
+
+		if(vcr.mode == InputVCRMode.Record)
+			vcr.Record();
+	}
+
+    void Update ()
+    {
+
+		if (vcr.GetButtonDown("Jump")) 
+			jump = true;
+
+		if (Input.GetKey ("r"))
+			vcr.Record ();
+
+		if(Input.GetKey("t"))
+			vcr.Stop();
+		if(Input.GetKey("y"))
+			vcr.Play();
+
+    }
+
+	void FixedUpdate()
+	{
+		// Read the inputs.
+		bool crouch = Input.GetKey(KeyCode.LeftControl);
+		float h = vcr.GetAxis("Horizontal");
+
+		// Pass all parameters to the character control script.
+		character.Move( h, crouch , jump );
+
+        // Reset the jump input once it has been used.
+	    jump = false;
+	}
+}
---
+++ Assets/Scripts/Respawner.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Respawner : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		other.transform.position = Vector3.zero;
+	}
+}
