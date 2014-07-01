9092165782e09f1004d2c0fd0089dec2821373c0
Morifu <morifek@gmail.com>
2014-07-01 10:53:03 +0200

    Folder structure reorganization

--- Assets/Scripts/Respawner.cs
+++ Assets/Scripts/Respawner.cs
@@ -3,18 +3,16 @@ using System.Collections;
 
 public class Respawner : MonoBehaviour {
 
+	public Transform spawnPoint;
+
 	// Use this for initialization
 	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+		if(spawnPoint == null)
+			spawnPoint.position = Vector3.zero;
 	}
 
 	void OnTriggerEnter2D(Collider2D other)
 	{
-		other.transform.position = Vector3.zero;
+		other.transform.position = spawnPoint.position;
 	}
 }
